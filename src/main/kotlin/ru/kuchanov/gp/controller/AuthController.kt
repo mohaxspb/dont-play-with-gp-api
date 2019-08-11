6cb0a6930e4a2c8c1dcea16bfd1793061db2897c
mohax <mohax.spb@gmail.com>
2019-08-11 22:01:28 +0300

    Realize web auth (Cookie in Session) while register user. Change input params for register method. Separate methods to generate Authentication and AccessToken from it.

--- src/main/kotlin/ru/kuchanov/gp/controller/AuthController.kt
+++ src/main/kotlin/ru/kuchanov/gp/controller/AuthController.kt
@@ -3,23 +3,24 @@ package ru.kuchanov.gp.controller
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.oauth2.common.OAuth2AccessToken
+import org.springframework.security.oauth2.provider.ClientDetailsService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import ru.kuchanov.gp.GpConstants
-import ru.kuchanov.gp.bean.auth.AuthorityType
-import ru.kuchanov.gp.bean.auth.GpUser
-import ru.kuchanov.gp.bean.auth.UserAlreadyExistsException
-import ru.kuchanov.gp.bean.auth.UsersAuthorities
+import ru.kuchanov.gp.bean.auth.*
 import ru.kuchanov.gp.service.auth.AuthService
 import ru.kuchanov.gp.service.auth.GpUserDetailsService
 import ru.kuchanov.gp.service.auth.UsersAuthoritiesService
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @RestController
 @RequestMapping("/" + GpConstants.Path.AUTH + "/")
 class AuthController @Autowired constructor(
     val passwordEncoder: PasswordEncoder,
+    val clientDetailsService: ClientDetailsService,
     val authService: AuthService,
     val usersService: GpUserDetailsService,
     val usersAuthoritiesService: UsersAuthoritiesService
@@ -30,9 +31,19 @@ class AuthController @Autowired constructor(
         @RequestParam(value = "email") email: String,
         @RequestParam(value = "password") password: String,
         @RequestParam(value = "fullName") fullName: String,
-        @RequestParam(value = "avatarUrl") avatarUrl: String? = null,
-        @RequestParam(value = "clientId") clientId: String
+        //todo enum for language
+        @RequestParam(value = "primaryLanguage", defaultValue = "EN") primaryLanguage: String = "EN",
+        @RequestParam(value = "clientId") clientId: String,
+        @RequestParam(value = "targetUrlParameter") targetUrlParameter: String?,
+        httpServletResponse: HttpServletResponse,
+        httpServletRequest: HttpServletRequest
     ): OAuth2AccessToken? {
+        //check client existing
+        try {
+            clientDetailsService.loadClientByClientId(clientId)
+        } catch (e: ClientNotFoundError) {
+            throw e
+        }
         //check if user already exists
         val userInDb = usersService.loadUserByUsername(email)
         if (userInDb != null) {
@@ -42,8 +53,6 @@ class AuthController @Autowired constructor(
             GpUser(
                 username = email,
                 password = passwordEncoder.encode(password),
-                avatar = avatarUrl,
-                userAuthorities = setOf(),
                 fullName = fullName
             )
         )
@@ -53,6 +62,13 @@ class AuthController @Autowired constructor(
         //todo send email with password
 //        emailService.sendEmail(email, REGISTRATION_EMAIL_SUBJECT, "Your password is:\n$password")
 
-        return authService.getAccessTokenForUsernameAndClientId(email, clientId)
+        val auth = authService.generateAuthenticationForUsernameAndClientId(email, clientId)
+        return if (targetUrlParameter != null) {
+            authService.authenticateRequest(auth, httpServletRequest)
+            httpServletResponse.sendRedirect(targetUrlParameter)
+            null
+        } else{
+            authService.getAccessTokenFromAuthentication(auth)
+        }
     }
 }
