60547b3662b19b00bf13736030680887bf0fd0e2
mohax <mohax.spb@gmail.com>
2019-10-18 19:32:39 +0300

    Refactoring.

--- src/main/kotlin/ru/kuchanov/gp/model/dto/data/ArticleIdAndCommentsCount.kt
+++ src/main/kotlin/ru/kuchanov/gp/model/dto/data/ArticleIdAndCommentsCount.kt
@@ -1,11 +1,7 @@
 package ru.kuchanov.gp.model.dto.data
 
-import org.springframework.beans.factory.annotation.Value
-
 interface ArticleIdAndCommentsCount {
 
-//    @Value("#{target.article_id}")
     fun getArticleId(): Long
-//    @Value("#{target.count}")
     fun getCommentsCount(): Long
 }
